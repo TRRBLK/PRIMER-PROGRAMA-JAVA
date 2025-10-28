1f9f842a61d0fafa5204c1fd1f24b07f6d9f1bdf
Jorge T <git.trrblnc@gmail.com>
2025-10-28 11:43:39 -0600

    SECOND VERSION WITH CONTROLLED LOOPS

--- GestorDeTareas.java
+++ GestorDeTareas.java
@@ -4,44 +4,53 @@ public class GestorDeTareas {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int opcion;
         int TotalPrioridad = 0;
 
+        do {
         System.out.println("== GESTOR DE TAREAS ==");
         System.out.println("1. AGREGAR TAREA");
         System.out.println("2. VER TAREAS (PENDIENTE)");
         System.out.println("3. SALIR");
-
         System.out.print("SELECCIONA UNA DE LAS OPCIONES DEL 1 AL 3: ");
-        int opcion = sc.nextInt();
+        opcion = sc.nextInt();
         sc.nextLine();
 
-        if(opcion >=1 && opcion <=3) {
-            if (opcion == 1) {
+       switch (opcion) {
+           case (1):
                 System.out.print("¿QUE TAREA DESEAS AGREGAR?: ");
                 String nombreTarea = sc.nextLine();
 
                 System.out.print("ASIGNA LA PRIORIDAD DE LA TAREA DEL 1 AL 5: ");
+
                 int prioridad = sc.nextInt();
+                sc.nextLine();
 
-
-                boolean validez = prioridad >=1 && prioridad <=5;
-                System.out.println("VALIDANDO " + validez);
-
+                if (prioridad>=1 && prioridad<=5) {
                 TotalPrioridad += prioridad;
-
-
-                System.out.println("TAREA AGREGADA: " + nombreTarea + " 2prioridad: " + prioridad);
+                System.out.println("TAREA AGREGADA: " + nombreTarea + " con prioridad:" + prioridad + " agregada");
                 System.out.println("SUMA TOTAL DE PRIORIDADES: " + TotalPrioridad);
-                
-            } else if (opcion == 2) {
+                } else {
                 System.out.println("FUNCION AUN NO IMPLENTADA");
-            } else {
-                System.out.println("SALIENDO");
             }
-        } else {
-            System.out.println("OPCION NO VALIDA");
+        break;
+
+           case (2):
+               System.out.println("FUNCION AUN NO IMPLEMENTADA");
+               break;
+
+           case (3):
+               System.out.println("SALIENDO");
+               break;
+
+           default:
+               System.out.println("OPCION NO VALIDA. INTENTA DE NUEVO");
+               break;
+
         }
+            } while (opcion != 3);
 
         sc.close();
     }
+
 }
